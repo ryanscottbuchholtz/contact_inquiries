fa01acf420dce11720044b404e2646095a32001c
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2013-12-30 12:18:43 -0500

    Initial commit.  Gemfile and confid set.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactForm::Application.config.secret_key_base = '8c3ca43e7f46fb78646de1c6150a83763213909d75223d330c2556bd3aa28ee97e5967293f0b5ee684e0eaff4356a9e4a43ad9ae586f50294c60510f09072396'
