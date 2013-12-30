ea0d70dfdcd7281ff3272646b8fb8fe946381226
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2013-12-30 15:15:30 -0500

    FactoryGirl-esq testing of Inquiry populating database

--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -10,4 +10,15 @@ describe Inquiry do
     it { should_not have_valid(:first_name).when(nil) }
     it { should_not have_valid(:last_name).when(nil) }
   end
+
+  describe 'contact inquiry count' do
+    it 'returns the number of contact inquiries' do
+      Inquiry.create!(email_address: 'ryan@buchholtz.com', subject: 'First inquiry', description: 'Testing one', first_name: 'Ryan', last_name: 'Buchholtz')
+      Inquiry.create!(email_address: 'doug@munn.com', subject: 'Second inquiry', description: 'Testing two', first_name: 'Doug', last_name: 'Munn')
+
+      expect(Inquiry.count).to equal(2)
+      expect(Inquiry.first.email_address).to eql('ryan@buchholtz.com')
+    end
+  end
+
 end
